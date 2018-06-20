e47d186239ced462b9b98664edeea1c3be097311
Milan Ricoul <mricoul@beapi.fr>
2018-06-20 19:51:33 +0200

    Better Regex

--- js/correct.js
+++ js/correct.js
@@ -1,7 +1,7 @@
-var textNode, walk=document.createTreeWalker(document,NodeFilter.SHOW_TEXT,null,false);
-while(textNode=walk.nextNode()) {
+var textNode, walk = document.createTreeWalker(document, NodeFilter.SHOW_TEXT, null, false);
+while (textNode = walk.nextNode()) {
     textNode.nodeValue = textNode.nodeValue
-    .replace(/Emmanuel Macron/gi, 'Manu')
-    .replace(/le Président de la République/gi, 'Manu')
-    .replace(/Monsieur le Président/gi, 'Manu')
+        .replace(/^(?=.*\Emmanuel\b)(?=.*\bMacron\b).*$/gi, 'Manu')
+        .replace(/^(?=.*\Le\b)(?=.*\bprésident\b)(?=.*\bde\b)(?=.*\bla\b)(?=.*\bRépublique\b).*$/gi, 'Manu')
+        .replace(/^(?=.*\Monsieur\b)(?=.*\ble\b)(?=.*\bprésident\b).*$/gi, 'Manu')
 }
