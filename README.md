7e72d8442d835321c2863bb63138643607705e83
Maxime Bouveron <maxime.bouveron@gmail.com>
2018-06-20 15:31:05 +0200

    Intial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Manu Auto Correct
+
+Bon alors déjà c'est M. Le Président.
+
+![OSS 117](oss.jpg)
+
+Le meilleur moyen d'utiliser cette extension est de passer par le [Chrome Webstore](https://example.com)
+
+Vous pouvez aussi télécharger ce dépot et le charger comme "unpacked extension" dans Chrome
---
+++ js/correct.js
@@ -0,0 +1,7 @@
+var textNode, walk=document.createTreeWalker(document,NodeFilter.SHOW_TEXT,null,false);
+while(textNode=walk.nextNode()) {
+    textNode.nodeValue = textNode.nodeValue
+    .replace(/Emmanuel Macron/gi, 'Manu')
+    .replace(/le Président de la République/gi, 'Manu')
+    .replace(/Monsieur le Président/gi, 'Manu')
+}
