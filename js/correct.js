bc9ebf0b3e9468f0dc3dc7c614d5d69693231cfa
Clement Foyer <clement.foyer@gmail.com>
2018-06-22 00:53:19 +0100

    Extraction de la sous-string de reconnaissance du president
    La sous string extraite permet de réutiliser la string pour la seconde
    regexp utile pour détecter les formes telles que "du président" tel que
    mentionné dans la PR #12
    
    Ajout de check de bornes de mots (notamment important pour les "M." ou
    "Mr".

--- js/correct.js
+++ js/correct.js
@@ -1,19 +1,22 @@
 var textNode,
   walk = document.createTreeWalker(document, NodeFilter.SHOW_TEXT, null, false)
-const rExpManu = new RegExp('emmanuelmacron|' +
-    '(?:\bm.?|monsieur|[eé]mmanuel(?: jean-michel fr[eé]d[eé]ric)?) macron|' +
-    '(?:\bm.? |monsieur )?le pr[ée]sident' +
+const sPres = 'pr[ée]sident' +
         '(?: de la r[ée]publique(?: fran[çc]aise)?|' +
-        ' macron| fran[çc]ais)?(?! [(]| de (?!la|f))|' +
+        ' (?:[eé]mmanuel )?macron| fran[çc]ais)?(?! [(]| de (?!la|f))'
+const rExpPres = new RegExp('du(?= ' + sPres + ')', 'gi')
+const rExpManu = new RegExp('emmanuel(?: |)macron|' +
+    '(?:\bm.?|monsieur|[eé]mmanuel(?: jean-michel fr[eé]d[eé]ric)?) macron|' +
+    '(?:\bm.? |monsieur )?le ' + sPres + '|' +
     'the (?:president of france|french president)'
         , 'gi')
 
 while ((textNode = walk.nextNode())) {
   textNode.nodeValue = textNode.nodeValue
-    .replace(/((?:d|qu))['’][eé]mmanuel macron/gi, '$1e Manu')
-    .replace(/((?:d|qu)e) macron/gi, '$1 Manu')
+    .replace(/(\b(?:d|qu))['’](?=[eé]mmanuel macron)/gi, '$1e ')
+    .replace(/(\b(?:d|qu)e) macron/gi, '$1 Manu')
     .replace(/[eé]mmanuel et brigitte macron/gi, 'Manu et Brigitte')
     .replace(/brigitte et [eé]mmanuel macron/gi, 'Brigitte et Manu')
     .replace(/couple macron/gi, 'couple à Manu')
+    .replace(rExpPres, 'de le')
     .replace(rExpManu, 'Manu')
 }
