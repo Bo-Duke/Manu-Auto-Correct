fef9dd66daf50df22b43071128d93430a2fc89a0
Kameyu <tristan13.d@gmail.com>
2018-06-21 16:37:32 +0200

    More precision and added Brigitte
    Added a better precision with space checking on the name (for @EmmanuelMacron on twitter),
    took Brigitte into account too.

--- js/correct.js
+++ js/correct.js
@@ -1,14 +1,21 @@
 var textNode, walk=document.createTreeWalker(document,NodeFilter.SHOW_TEXT,null,false);
+var rManu = new RegExp('Emmanuel( |)Macron|'+
+	'le Pr[eé]sident de la R[eé]publique fran[cç]aise|'+
+	'Pr[eé]sident de la R[eé]publique fran[cç]aise|'+
+	'le Pr[eé]sident de la R[eé]publique|'+
+	'du Pr[eé]sident de la R[eé]publique|'+
+	'M. le Pr[eé]sident de la R[eé]publique|'+
+	'Monsieur le Pr[eé]sident de la R[eé]publique|'+
+	'Monsieur le Pr[eé]sident|'+
+	'M. Macron', 'gi')
+	
+var rBoth = new RegExp('Emmanuel et Brigitte Macron|'+
+	'Brigitte et Emmanuel Macron|'+
+	'Emmanuel Macron et Brigitte Macron|'+
+	'M. et Mme Macron|'+
+	'Mme et M. Macron', 'gi')
+	
 while(textNode=walk.nextNode()) {
-	var rExp = new RegExp('Emmanuel Macron|'+
-	'M. le Président de la République|'+ // Keep the dot in case of "Mr" or "M."
-	'le Président de la République|'+ // Put this one after, so that the one above matches first
-	'Monsieur le Président de la République|'+
-	'Monsieur le Président|'+
-	'Président de la République française|'+
-	'Président de la République|' +
-	'Emmanuel Jean-Michel Frédéric Macron|',
-	'M. Macron', 'gi');
-
-    textNode.nodeValue = textNode.nodeValue.replace(rExp, 'Manu')
+    textNode.nodeValue = textNode.nodeValue.replace(rManu, 'Manu')
+	textNode.nodeValue = textNode.nodeValue.replace(rBoth, 'Manu et Brigitte')
 }
