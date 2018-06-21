8f9fdcd13e20aefa590da4a60cdc23097fdccb58
Clément Foyer <clement.foyer@gmail.com>
2018-06-21 14:18:18 +0100

    Creation d'une seule RegEx principe pour 'Manu'
    Le remplacement par le mot 'Manu' se fait pour la majorité des
    références au président de la république française (ou toutes les
    formules raccourcies aussi), avec ou sans accent, avec ou sans
    cédille. Prise en compte des versions raccourcies de "monsieur" aussi.
    
    Ajout de deux formules anglophones. Ça pourrait être enrichi
    ultérieurement avec d'autre langues.
    
    Prise en compte de l'apostrophe (à la place de la simple quote qui est
    toujours présente, mais sans doute un peu inutile ici).
    
    Prise en compte de l'issue #1 avec la prise en compte du couple. Ici
    double référence, en disant bien "couple *à*" en référence au tollé de
    la "fête à macron" ; on conserve la notion de couple plutôt que de
    remplacer par juste "Manu" qui exclu Brigitte (qui n'a rien demandé, la
    pauvre), et si on remplaçait par "Manu et Brigitte", il aurait fallu
    accorder les verbes en conséquence, et c'est pas le but ici.

--- js/correct.js
+++ js/correct.js
@@ -1,16 +1,18 @@
-var textNode, walk=document.createTreeWalker(document,NodeFilter.SHOW_TEXT,null,false);
-const rExp = new RegExp('Emmanuel Macron|'+
-	'EmmanuelMacron|'+
-	'M. le Président de la République|'+ // Keep the dot in case of "Mr" or "M."
-	'le Président de la République|'+ // Put this one after, so that the one above matches first
-	'Monsieur le Président de la République|'+
-	'Monsieur le Président|'+
-	'Président de la République française|'+
-	'Président de la République|' +
-	'Le Président Macron|' +
-	'Emmanuel Jean-Michel Frédéric Macron|' +
-	'M. Macron', 'gi');
+var textNode,
+  walk = document.createTreeWalker(document, NodeFilter.SHOW_TEXT, null, false)
+const rExpManu = new RegExp('emmanuelmacron|' +
+    '(?:m.?|monsieur|[eé]mmanuel(?: jean-michel fr[eé]d[eé]ric)?) macron|' +
+    '(?:m.? |monsieur )?le pr[ée]sident(?: de la r[ée]publique' +
+        '(?: fran[çc]aise| macron| fran[çc]ais)?)?|' +
+    'the (?:president of france|french president)|' +
+    'macron' /* à garder en dernière regex */
+        , 'gi')
 
-while(textNode=walk.nextNode()) {
-    textNode.nodeValue = textNode.nodeValue.replace(rExp, 'Manu');
+while ((textNode = walk.nextNode())) {
+  textNode.nodeValue = textNode.nodeValue
+    .replace(/d['’][eé]mmanuel macron/gi, 'de Manu')
+    .replace(/[eé]mmanuel et brigitte macron/gi, 'Manu et Brigitte')
+    .replace(/brigitte et [eé]mmanuel macron/gi, 'Brigitte et Manu')
+    .replace(/couple macron/gi, 'couple à Manu')
+    .replace(rExpManu, 'Manu')
 }
