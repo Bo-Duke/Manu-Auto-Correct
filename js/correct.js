96720e5f85dfaa236ce2257d968b906ba642d115
Clement Foyer <clement.foyer@gmail.com>
2018-06-21 15:17:29 +0100

    Ne pas remplacer lors d'un président de parti/région/asso
    En partie en raison de la page indiquée dans l'issue#2 où était indiqué
    "le président (LR) de ...".
    
    Prise en compte un peu plus propre des apostrophes pour d' et qu'
    
    retirer la dernière condition de changement sur juste "Macron" pour
    eviter que "Brigitte Macron" ne devienne "Brigitte Manu". Ça manque de
    negative look-behind pour faire ça bien je crois...

--- js/correct.js
+++ js/correct.js
@@ -1,16 +1,17 @@
 var textNode,
   walk = document.createTreeWalker(document, NodeFilter.SHOW_TEXT, null, false)
 const rExpManu = new RegExp('emmanuelmacron|' +
-    '(?:m.?|monsieur|[eé]mmanuel(?: jean-michel fr[eé]d[eé]ric)?) macron|' +
-    '(?:m.? |monsieur )?le pr[ée]sident(?: de la r[ée]publique' +
-        '(?: fran[çc]aise| macron| fran[çc]ais)?)?|' +
-    'the (?:president of france|french president)|' +
-    'macron' /* à garder en dernière regex */
+    '(?:\bm.?|monsieur|[eé]mmanuel(?: jean-michel fr[eé]d[eé]ric)?) macron|' +
+    '(?:\bm.? |monsieur )?le pr[ée]sident' +
+        '(?: de la r[ée]publique(?: fran[çc]aise)?|' +
+        ' macron| fran[çc]ais)?(?! [(]| de (?!la|f))|' +
+    'the (?:president of france|french president)'
         , 'gi')
 
 while ((textNode = walk.nextNode())) {
   textNode.nodeValue = textNode.nodeValue
-    .replace(/d['’][eé]mmanuel macron/gi, 'de Manu')
+    .replace(/((?:d|qu))['’][eé]mmanuel macron/gi, '$1e Manu')
+    .replace(/((?:d|qu)e) macron/gi, '$1 Manu')
     .replace(/[eé]mmanuel et brigitte macron/gi, 'Manu et Brigitte')
     .replace(/brigitte et [eé]mmanuel macron/gi, 'Brigitte et Manu')
     .replace(/couple macron/gi, 'couple à Manu')
