cfece26b393dc38b90e1c4c063bfa13ad34807e2
mcsky <kevin.grenier17@orange.fr>
2018-06-21 15:45:07 +0200

    Declare the regex outside the while
    Improve the performance, the while loop can be executed many times

--- js/correct.js
+++ js/correct.js
@@ -1,11 +1,12 @@
 var textNode, walk=document.createTreeWalker(document,NodeFilter.SHOW_TEXT,null,false);
-while(textNode=walk.nextNode()) {
-	var rExp = new RegExp('Emmanuel Macron|'+
+const rExp = new RegExp('Emmanuel Macron|'+
 	'M. le Président de la République|'+ // Keep the dot in case of "Mr" or "M."
 	'le Président de la République|'+ // Put this one after, so that the one above matches first
 	'Monsieur le Président de la République|'+
 	'Monsieur le Président|'+
 	'Président de la République française|'+
-	'Président de la République', 'gi')
-    textNode.nodeValue = textNode.nodeValue.replace(rExp, 'Manu')
+	'Président de la République', 'gi');
+
+while(textNode=walk.nextNode()) {
+    textNode.nodeValue = textNode.nodeValue.replace(rExp, 'Manu');
 }
