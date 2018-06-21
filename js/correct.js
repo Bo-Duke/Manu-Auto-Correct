c11a16e535bf56bf715a20b9055bd26d0d4b635f
Gougere89 <clement@vlsk.eu>
2018-06-21 17:18:38 +0200

    Added few strings+case insensitive feature

--- js/correct.js
+++ js/correct.js
@@ -1,14 +1,16 @@
 var textNode, walk=document.createTreeWalker(document,NodeFilter.SHOW_TEXT,null,false);
 while(textNode=walk.nextNode()) {
-	var rExp = new RegExp('Emmanuel Macron|'+
-	'M. le Président de la République|'+ // Keep the dot in case of "Mr" or "M."
-	'le Président de la République|'+ // Put this one after, so that the one above matches first
-	'Monsieur le Président de la République|'+
-	'Monsieur le Président|'+
-	'Président de la République française|'+
-	'Président de la République|' +
-	'Emmanuel Jean-Michel Frédéric Macron|',
-	'M. Macron', 'gi');
+	var rExp = new RegExp('Emmanuel Macron.*|'+
+	'M. le Président de la République.*|'+ // Keep the dot in case of "Mr" or "M."
+	'le Président de la République.*|'+ // Put this one after, so that the one above matches first
+	'Monsieur le Président de la République.*|'+
+	'Monsieur le Président.*|'+
+	'Président de la République française.*|'+
+	'Président de la République.*|' +
+	'le Chef de l\'Etat français.*|' +
+	'le candidat d\'En Marche.*|' +
+	'Emmanuel Jean-Michel Frédéric Macron.*|',
+	'M. Macron.*|', 'gi');
 
     textNode.nodeValue = textNode.nodeValue.replace(rExp, 'Manu')
 }
