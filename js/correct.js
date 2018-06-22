3795cbf42e4d262c67970f4e7880eb8ee98d7c6b
Clement Foyer <clement.foyer@gmail.com>
2018-06-22 10:10:49 +0100

    Corrige l'usage des bornes de mots
    J'avais oublié que la construction de la regex se faisait via une string
    et donc qu'il fallait échapper les backslashs.
    
    Ajout de prise en compte d'espaces insécables, ou de tabulations ou
    autres espaces blancs. Prise en compte d'espacement multiples (un double
    espace qui trainerait par exemple). Fix #6.

--- js/correct.js
+++ js/correct.js
@@ -1,22 +1,22 @@
 var textNode,
   walk = document.createTreeWalker(document, NodeFilter.SHOW_TEXT, null, false)
 const sPres = 'pr[ée]sident' +
-        '(?: de la r[ée]publique(?: fran[çc]aise)?|' +
-        ' (?:[eé]mmanuel )?macron| fran[çc]ais)?(?! [(]| de (?!la|f))'
-const rExpPres = new RegExp('du(?= ' + sPres + ')', 'gi')
-const rExpManu = new RegExp('emmanuel(?: |)macron|' +
-    '(?:\bm.?|monsieur|[eé]mmanuel(?: jean-michel fr[eé]d[eé]ric)?) macron|' +
-    '(?:\bm.? |monsieur )?le ' + sPres + '|' +
-    'the (?:president of france|french president)'
+        '(?:\\s+de\\s+la\\s+r[ée]publique(?:\\s+fran[çc]aise)?|' +
+        '\\s+(?:[eé]mmanuel\\s+)?macron|\\s+fran[çc]ais)?(?!\\s+[(]|\\s+de\\s+(?!la|f))'
+const rExpPres = new RegExp('du(?=\\s+' + sPres + ')', 'gi')
+const rExpManu = new RegExp('emmanuel(?:\\s+|)macron|' +
+    '(?:\\bm.?|monsieur|[eé]mmanuel(?:\\s+jean-michel\\s+fr[eé]d[eé]ric)?)\\s+macron|' +
+    '(?:\\bm.?\\s+|monsieur\\s+)?le\\s+' + sPres + '|' +
+    'the\\s+(?:president\\s+of\\s+france|french\\spresident)'
         , 'gi')
 
 while ((textNode = walk.nextNode())) {
   textNode.nodeValue = textNode.nodeValue
     .replace(/(\b(?:d|qu))['’](?=[eé]mmanuel macron)/gi, '$1e ')
-    .replace(/(\b(?:d|qu)e) macron/gi, '$1 Manu')
-    .replace(/[eé]mmanuel et brigitte macron/gi, 'Manu et Brigitte')
-    .replace(/brigitte et [eé]mmanuel macron/gi, 'Brigitte et Manu')
-    .replace(/couple macron/gi, 'couple à Manu')
+    .replace(/(\b(?:d|qu)e)\s+macron/gi, '$1 Manu')
+    .replace(/[eé]mmanuel\s+et\s+brigitte macron/gi, 'Manu et Brigitte')
+    .replace(/brigitte\s+et\s+[eé]mmanuel\s+macron/gi, 'Brigitte et Manu')
+    .replace(/couple\s+macron/gi, 'couple à Manu')
     .replace(rExpPres, 'de le')
     .replace(rExpManu, 'Manu')
 }
