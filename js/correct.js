8b7e01f5cf3e2ff4106f22a9a4b30340b4fafebe
Maxime Bouveron <maxime.bouveron@gmail.com>
2018-06-22 23:37:34 +0200

    Added support for twitter handle

--- js/correct.js
+++ js/correct.js
@@ -1,5 +1,6 @@
 var textNode, walk=document.createTreeWalker(document,NodeFilter.SHOW_TEXT,null,false);
 const rExp = new RegExp('Emmanuel Macron|'+
+	'EmmanuelMacron|'+
 	'M. le Président de la République|'+ // Keep the dot in case of "Mr" or "M."
 	'le Président de la République|'+ // Put this one after, so that the one above matches first
 	'Monsieur le Président de la République|'+
