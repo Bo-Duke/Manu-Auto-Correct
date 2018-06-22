800dfc3ce24a4e2ca77405aabe413e883996417f
Clément <clement.guibout@gmail.com>
2018-06-22 21:16:01 +0200

    Fix
    Added "Le président macron" for news website (this designation is very often used by news websites) :D

--- js/correct.js
+++ js/correct.js
@@ -7,6 +7,7 @@ while(textNode=walk.nextNode()) {
 	'Monsieur le Président|'+
 	'Président de la République française|'+
 	'Président de la République|' +
+	'Le Président Macron|'+
 	'Emmanuel Jean-Michel Frédéric Macron|',
 	'M. Macron', 'gi');
 
