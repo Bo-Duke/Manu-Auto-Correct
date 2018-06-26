7fd57d41287067642e8919cc85633f9d251e210d
Clement Foyer <cfoyer@cray.com>
2018-06-26 10:45:48 +0200

    Fix issue #20. Ajout remplacement "au president" par "à Manu"

--- js/correct.js
+++ js/correct.js
@@ -6,7 +6,10 @@ function textReplaceManu(str) {
         'pr[ée]sident emmanuel macron|'+
         'pr[ée]sident macron'
     /* This is just "catch *DU* if followed by "président etc..." formula */
-    const rExpDuPres = new RegExp('du(?=\\s+' + strPres.replace(/ /g, '\\s+') + ')', 'gi')
+    const rExpDuPres = new RegExp('\\bdu(?=\\s+' + strPres.replace(/ /g, '\\s+') + ')', 'gi')
+    /* This is just "catch *AU* if followed by "président etc..." formula. Notice the "\\b" at the
+     * begining. It ensures "au" is a stand alone word. Do not remove it. */
+    const rExpAuPres = new RegExp('\\bau(?=\\s+' + strPres.replace(/ /g, '\\s+') + ')', 'gi')
     /* Build replacement regex */
     const strManu = 'EmmanuelMacron|'+
 		'M[\\.r] le Pr[eé]sident de la R[eé]publique fran[cç]aise|'+ // Keep the dot in case of "Mr" or "M."
@@ -39,15 +42,17 @@ function textReplaceManu(str) {
      */
     const rExpManu = new RegExp(strManu.replace(/ /g,'\\s+'),  'gi');
 
-	/* Apply replacement in the text */
+	/* Apply replacement in text */
 	/* 1) replace "d'Emmanuel Macron" with "de Manu". Same with "qu'Emmanuel macron"
 	 * 2) replace "de Macron" or "que Macron" with "de Manu" or "que Manu" respectively
      * 3) replace "Emmanuel et Brigitte Macron" with "Manu et Brigitte'
      * 4) same as 3), but with Brigitte first
      * 5) replace "couple macron" and "couple présidentiel" with "couple à Manu"
      * 6) replace "du président etc" with "de le prédisent etc...", in order to have
-     *      "le président etc..." replace with "Manu" next
-     * 7) replace all references to "président de la republique etc..." by "Manu"
+     *      "le président etc..." replace with "Manu" later
+     * 6) replace "au président etc" with "à le prédisent etc...", in order to have
+     *      "le président etc..." replace with "Manu" later
+     * 8) replace all references to "président de la republique etc..." by "Manu"
 	 */
 	return str.replace(/(\b(?:d|qu))['’][eé]mmanuel macron/gi, '$1e Manu')
 		.replace(/(\b(?:d|qu)e) macron/gi, '$1 Manu')
@@ -55,6 +60,7 @@ function textReplaceManu(str) {
 		.replace(/brigitte et [eé]mmanuel macron/gi, 'Brigitte et Manu')
 		.replace(/couple macron|couple pr[eé]sidentiel/gi, 'couple à Manu')
 		.replace(rExpDuPres, 'de le')
+		.replace(rExpAuPres, 'à le')
 		.replace(rExpManu, 'Manu')
 }
 
